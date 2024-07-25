2272eee26ee315a307476e9f57a6f7cb294fe2c5
ganggaa <ganggaputra@refactory.id>
2024-07-25 11:34:51 +0700

    Create function.dart

---
+++ function.dart
@@ -0,0 +1,12 @@
+void main() {
+  print(perkalian(2, 5));
+  print(pembagian(10, 2));
+}
+
+int perkalian(number1, number2) {
+  return number1 * number2;
+}
+
+int pembagian(number1, number2) {
+  return number1 / number2;
+}
